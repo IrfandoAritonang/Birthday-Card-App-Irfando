3b386d8aea0c6bd427b70ea89bf416fa87f108be
Irfando <irfandoaritonang86@gmail.com>
2023-08-14 01:37:57 +0700

    add image

--- app/src/main/java/com/example/birthdaycardapp/MainActivity.kt
+++ app/src/main/java/com/example/birthdaycardapp/MainActivity.kt
@@ -3,14 +3,24 @@ package com.example.birthdaycardapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.birthdaycardapp.ui.theme.BirthdayCardAppTheme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,28 +29,65 @@ class MainActivity : ComponentActivity() {
             BirthdayCardAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    /*modifier = Modifier.fillMaxSize(),*/
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    GreetingImage("James", from = "~ From Irfando")
                 }
             }
         }
     }
 }
 
+/*#pertama*/
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun GreetingText(name: String, from : String, modifier: Modifier = Modifier) {
+    Column (
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier.padding(8.dp))
+    {
+        Text(
+            text = "Happy Birthday $name :)",
+            fontSize = 87.sp,
+            lineHeight = 116.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = from,
+            fontSize = 36.sp,
+            modifier = Modifier
+                .padding(top = 16.dp)
+                .padding(end = 16.dp)
+                .align(alignment = Alignment.End)
+        )
+    }
 }
 
+/*#Ketiga*/
+/*menambahkan gambar secara composable*/
+@Composable
+fun GreetingImage(name: String, from: String, modifier: Modifier = Modifier) {
+    /*cara memanggil gambar*/
+    //Step nextcreate a box to overlap image and texts
+    val image = painterResource(R.drawable.androidparty)
+    Box {
+        Image(
+            painter = image,
+            contentDescription = null
+        )
+        GreetingText(
+                name = name,
+                from = from,
+                modifier = Modifier.fillMaxSize().padding(0.dp)
+        )
+    }
+}
+
+/*#Kedua*/
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun BirthdayCardPreview() {
     BirthdayCardAppTheme {
-        Greeting("Android")
+        GreetingImage("James", from = "~ From Irfando")
     }
 }
